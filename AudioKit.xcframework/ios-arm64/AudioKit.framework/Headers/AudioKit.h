fd42dde3fe5acf1521a3baa5f07d6c95b6b5b968
Venkatesh S <anilkumarunni@gmail.com>
2024-03-28 14:28:39 +0530

    test

---
+++ AudioKit.xcframework/ios-arm64/AudioKit.framework/Headers/AudioKit.h
@@ -0,0 +1,18 @@
+//
+//  AudioKit.h
+//  AudioKit
+//
+//  Created by Anil Kumar on 25/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioKit.
+FOUNDATION_EXPORT double AudioKitVersionNumber;
+
+//! Project version string for AudioKit.
+FOUNDATION_EXPORT const unsigned char AudioKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioKit/PublicHeader.h>
+
+
